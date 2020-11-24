c0faa3974808b0da7abfbd1639d789a5a53bda42
alexander <alexander.kopp@gmx.at>
2020-11-24 10:40:58 +0100

    removed unimportant stuff

--- marketplace-frontend/src/environments/environment.hmr.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  hmr: true,
-  REDIRECT_URI: "http://localhost:4200/main/profile",
-  JSON_SERVER_URL: "http://localhost:3000/repository",
-  CORE_URL: "http://localhost:8090",
-  CONFIGURATOR_URL: "http://localhost:4201"
-};
